a978b787f026101582e313ede93c61f8233b3a1a
henrikh.baghramyan <henrikh.baghramyan@jos-quantum.de>
2024-04-13 22:34:06 +0200

    added appendix mine

---
+++ appendix-A/01_main-chapter-code/appendix_A2.py
@@ -0,0 +1,13 @@
+import torch
+
+tensor0d = torch.tensor(1)
+
+tensor1d = torch.tensor([1, 2, 3])
+
+tensor2d = torch.tensor([[1, 2, 3], [4, 5, 6]])
+
+tensor3d = torch.tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+flaotvec = torch.tensor([1.0, 2.0, 3.0])
+
+print("done")
